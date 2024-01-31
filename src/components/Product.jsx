974853a98cfd9aabc82715fdfb834262dcd5ceb2
preranababar9 <preranababar9403@gmail.com>
2024-01-31 13:06:06 +0530

    card added and styled

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,24 +4,45 @@ import { CiShoppingCart } from "react-icons/ci";
 
 export const Product = () => {
         return (
-                <div className=' flex h-full w-full bg-[#F9D9AA] '>
+                <div className=' flex py-10 flex-col h-full w-full bg-[#F9D9AA] '>
                         <div className='py-16 px-8 '>
                                 <h1 className='text-3xl font-bold'>Popular <span className='underline-offset-1 under decoration-orange-100'>Now</span> </h1>
                         </div>
-                        <div className='px-6 py-16  h-full' >
-                                <img className='w-1/4 aspect-auto rounded-md'
-                                        src={ProductImg}
-                                        alt="Vanilla Latte"
-                                />
-                                <p className='text-2xl text-amber-900 font-semibold py-4'>Vanilla Latte                 21k</p>
-                                <button className='border-solid border-black text-lg font-semibold'>Hot</button>
-                                <button>Cold</button>
-                                <CiShoppingCart size={30} />
-                                <div>
+                        <div className='flex
+                     flex-wrap items-center justify-center gap-6 '>  <Card />
 
-                                </div>
+                                <Card title={'Vanilla Latte'} img={ProductImg} price={'21k'}/>
+                                <Card />
+                                <Card />
+                                <Card />
                         </div>
+
                 </div>
         )
 }
 export default Product;
+
+
+
+const Card = ({ img, title, price }) => {
+        return (
+                <div className='px-8 py-12 bg-white h-full w-[30%] rounded-xl' >
+                        <img className='w-full aspect-auto rounded-md'
+                                src={img}
+                                alt="Vanilla Late"
+                        />
+                        <div className='flex justify-between'> <p className='text-3xl text-amber-900 font-semibold py-4'>{title}              </p>
+                                <p className='text-3xl text-amber-900 font-semibold py-4'>{price}</p>
+                        </div>
+                        <div className='flex justify-between'><div className='flex justify-between'>
+                                <button className='border-solid border-black text-lg font-semibold'>Hot</button>
+                                <button>Cold</button>
+                        </div>
+
+                                <CiShoppingCart size={30} />
+                        </div>
+
+                </div>
+        )
+}
+
